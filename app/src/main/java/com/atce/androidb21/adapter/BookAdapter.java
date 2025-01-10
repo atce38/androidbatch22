391d60bc9434b7e63e227aa84a4620638a720880
atce38 <81683047+atce38@users.noreply.github.com>
2025-01-10 13:20:20 +0500

    Lecture 29 - Sqlite Database using SugarOrm Library - Edit, Update, Delete Record

--- app/src/main/java/com/atce/androidb21/adapter/BookAdapter.java
+++ app/src/main/java/com/atce/androidb21/adapter/BookAdapter.java
@@ -1,12 +1,18 @@
 package com.atce.androidb21.adapter;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.atce.androidb21.R;
@@ -33,14 +39,68 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Book app = appList.get(position);
-        holder.appName.setText(app.getTitle()+"\n"+app.getEdition());
+        holder.appName.setText(app.getTitle()+"\n"+app.getEdition()+"\n"+app.getId());
         // Set app icon using Glide or Picasso library
 //        Glide.with(holder.itemView.getContext())
 //                .load(app.getAppIcon())
 //                .diskCacheStrategy(DiskCacheStrategy.DATA)
 //                .into(holder.appIcon);
 //        Picasso.get().load(app.getAppIcon()).into(holder.appIcon);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(holder.itemView.getContext(), "ID:"+app.getId(), Toast.LENGTH_LONG).show();
+                showDialogtoUpdate(holder.itemView.getContext(),app);
+            }
+        });
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(holder.itemView.getContext());
+                builder.setTitle("Delete Book")
+                        .setMessage("Are you sure you want delete Book "+app.getTitle()+" ?")
+                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Book book=Book.findById(Book.class,app.getId());
+                                book.delete();
+                                Toast.makeText(holder.itemView.getContext(), "Book Deleted Successfully!", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                            }
+                        }).setNegativeButton("Cancel",null).show();
+                return true;
+            }
+        });
 
+    }
+
+    private void showDialogtoUpdate(Context context, Book book) {
+        LayoutInflater inflater=LayoutInflater.from(context);
+        View view=inflater.inflate(R.layout.update_sqlite_dialog,null);
+        AlertDialog.Builder builder=new AlertDialog.Builder(context);
+
+        EditText edtEdition=view.findViewById(R.id.edtEdition);
+        EditText edtTitle=view.findViewById(R.id.edtTitle);
+        Button btnSave=view.findViewById(R.id.btnSave);
+        edtTitle.setText(book.getTitle()+"");
+        edtEdition.setText(book.getEdition()+"");
+
+        builder.setView(view);
+        AlertDialog dialog=builder.create();
+        dialog.show();
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book updated=Book.findById(Book.class,book.getId());
+                updated.setTitle(edtTitle.getText().toString());
+                updated.setEdition(edtEdition.getText().toString());
+                updated.save();
+                Toast.makeText(context, "Book Updated Successfully!", Toast.LENGTH_SHORT).show();
+                edtTitle.setText("");
+                edtEdition.setText("");
+                dialog.dismiss();
+            }
+        });
     }
 
     @Override
